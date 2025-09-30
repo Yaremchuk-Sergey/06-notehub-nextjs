7048d9b90eed4f52197ac126de6975a8dac18f99
Yaremchuk-Sergey <bb01082014@gmail.com>
2025-09-30 12:08:24 +0300

    Fix build errors, ready for Vercel deployment

--- components/Header/page.tsx
+++ components/Header/page.tsx
@@ -17,7 +17,7 @@ const Header = () => {
             <Link href="/">Home</Link>
           </li>
           <li>
-            <a href="/notes">Notes</a>
+            <Link href="/notes">Notes</Link>
           </li>
         </ul>
       </nav>
